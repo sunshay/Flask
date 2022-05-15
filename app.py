86f5a782939e9702795cfc5e1ac142979913b2b1
sunshay <sunshayina@outlook.com>
2022-05-15 01:52:19 -0700

    another commit

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 
 app =  Flask(__name__)#application object as an instance of class Flask imported from the flask package
 #__name__ variable passed to the Flask class is a Python predefined variable
-@app.route("/")
+@app.route("/") # two strange @app.route lines above the function are decorators, a unique feature of the Python language
 def Hello():
     return "Hello world"
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000, debug=True)
-    
+
